5fc244c86eadb62f2f9f49d3489ec3117fce235b
Akki <akshatmadan2005@gmail.com>
2026-01-13 16:44:27 +1100

    Added CNN model implimentation

--- app/model.py
+++ app/model.py
@@ -4,7 +4,29 @@ from tensorflow.keras import layers, models
 import numpy as np
 
 
-def build_trafic_sign_cnn():
-    model = models.Sequential([
-        
-    ])
+def build_model(input_shape=(32, 32, 1), num_classes = 43):
+    model = model.Sequential()
+
+    model.add(layers.Conv2D(32,(3,3), activation='relu',
+                            input_shape=input_shape))
+    model.add(layers.MaxPooling2D((2,2)))
+
+    model.add(layers.Conv2D(64, (3,3), activation='relu'))
+    model.add(layers.MaxPooling2D((2,2)))
+
+    model.add(layers.Conv2D(128, (3,3), activation='relu'))
+    model.add(layers.MaxPooling2D((2,2)))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dropout(0.5))
+
+    model.add(layers.Dense(num_classes, activation='softmax'))
+
+    model.compile(
+        optimizer='adam',
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    return model
